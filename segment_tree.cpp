147b63e4730c0b6c44a33e5333a986026adeb6f4
Shubham Arora <shubhama1903@gmail.com>
2015-07-13 13:07:13 +0530

    segment tree without lazy propagation
    methods for addvalue() and multiplyvalue()
    in addition to rangesum()

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#define left 2*node
+#define right (2*node)+1
 using namespace std;
 
 class segtree //for range sum query
@@ -9,47 +11,99 @@ private:
 	vector<pair<long, long> > p;
 
 public:
-	segtree(long a[], long n)
+	segtree(vector<long> &a)
 	{
-		v.reserve(2*n);
-		p.reserve(2*n);
+	    long n=a.size();
+		v.resize(4*n+4);
+		p.resize(4*n+4);
 		maketree(a,1,0,n-1);
 	}
 
-	void maketree(long a[],long node, long i, long j)
+	void maketree(vector<long> &a,long node, long i, long j)
 	{
-		if(i==j) // leaf nodes , 
+		if(i==j) // leaf nodes ,
 		{
 			v[node]=a[i];
 			p[node]=make_pair(i,i);
 			return ;
 		}
 
-		maketree(a,(2*node),i,(i+j)/2);//build left tree
-		maketree(a,(2*node)+1,((i+j)/2)+1,j);//build right tree
-		v[node]=v[node*2]+v[(node*2)+1];	//add sum of both the trees
+		maketree(a,(left),i,(i+j)/2);//build left tree
+		maketree(a,right,((i+j)/2)+1,j);//build right tree
+		v[node]=v[left]+v[right];	//add sum of both the trees
 		p[node]=make_pair(i,j);
 	}
 
 	long rangesum(long i, long j)
-	{
-		return rsum(1,i,j);
-	}
+    {   return rsum(1,i-1,j-1); }
 
 	long rsum( long node, long i, long j)
 	{
-		if(p[node].first >j || p[node].second <i)
-			return 0;
+		if(p[node].first>j || p[node].second<i)//out of range
+            return 0;
 
-		if(p[node].first==i && p[node].second==j)
+		if(p[node].first>=i && p[node].second<=j)// all in range
 			return v[node];
 
-		long m=p[2*node].second;
-		if( j>m )
-			return rsum(2*node,i,m) + rsum((2*node)+1,m+1,j);
-
-		return rsum(2*node,i,j) + rsum(2*node+1,i,j);
+		return rsum(left,i,j) + rsum(right, i, j);
 	}
+
+	void addvalue(long i, long j, long diff)
+    {   addval(1,i-1,j-1, diff); }
+
+    void addval(long node, long i, long j, long diff) // perform basic(non-lazy) updation
+    {
+        if(p[node].first>j || p[node].second<i)// out of range
+            return ;
+
+        if(p[node].first==p[node].second)// if leaf node
+        {
+        	v[node]+=diff;
+        	return;
+        }
+
+        addval(left,i,j, diff);
+		addval(right, i, j, diff);
+		v[node]=v[left]+v[right];
+    }
+
+    void setval(long node, long i, long j, long val)
+    {
+    	if(p[node].first>j || p[node].second<i)// out of range
+            return ;
+
+        if(p[node].first==p[node].second)// if leaf node
+        {
+        	v[node]=val;
+        	return;
+        }
+
+        setval(left,i,j, val);
+		setval(right, i, j, val);
+		v[node]=v[left]+v[right];	
+    }
+
+    void setvalue(long i, long j, long val)
+    {setval(1,i-1,j-1,val);}
+
+    void multiplyvalue(long i, long j, long val)
+    { multiplyval( 1,i-1,j-1,val); }
+
+    void multiplyval(long node, long i, long j, long val)
+    {
+    	if(p[node].first>j || p[node].second<i)// out of range
+            return ;
+
+        if(p[node].first==p[node].second)// if leaf node
+        {
+        	v[node]*=val;
+        	return;
+        }
+
+        multiplyval(left,i,j, val);
+		multiplyval(right, i, j, val);
+		v[node]=v[left]+v[right];
+    }
 
 };
 
@@ -58,20 +112,23 @@ int main()
 {
 	std::ios_base::sync_with_stdio(false);
 
-	long n, a[100];
+	long n;
+	vector<long> a;
 	cout<<"How many array elements? : ";
 	cin>>n;
+	a.resize(n);
 	cout<<"Enter array elements: ";
 	for(long i=0; i<n; i++ )
-		cin>>a[n];
+		cin>>a[i];
 
-	segtree s(a,n);
-
+	segtree s(a);
+    //s.addvalue(2,3,4);
+    s.setvalue(1,1,5);
 	int i, j;
 	cout<<"Enter the range for which sum is required: ";
 	cin>>i>>j;
 
-	cout<<"the range sum is: "<<s.rangesum(i-1,j-1)<<'\n';
+	cout<<"the range sum is: "<<s.rangesum(i,j)<<'\n';
 
 	return 0;
 }
